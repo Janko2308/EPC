b5c9173d274e3371764ded3193a2e05f5516a848
Janko Hu <hu.janko@yahoo.de>
2023-09-25 19:20:05 +0200

    CopyConstructor and CopyAssignment + Test

--- include/technikumSTL/string.h
+++ include/technikumSTL/string.h
@@ -1,12 +1,21 @@
 #ifndef string_h
 #define string_h
 
+#include <cstddef>
+
 namespace technikum {
     class string {
     public:
         string(const char* cstr);
         ~string();
-        int length(char const* cstr) const;
+        //Copy Constructor
+        string(const string& str);
+        //Copy Assignment Operator
+        string& operator=(const string& str);
+        //Move Constructor
+        //Move Assignment Operator
+
+        size_t length(char const* cstr) const;
         //int length(string str) const;
         const char* c_str();
         void append(const char* cstr);
